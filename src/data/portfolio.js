160e735079a7c068f2aadeac48e70373dd900fc3
Bryan Tang <bryanktang@gmail.com>
2022-12-22 09:55:56 -0600

    BUGFIX: fixed missing image in production

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,13 +1,13 @@
 export default [
     {
         title: 'Algorithm Visualizer',
-        imgURL: '/assets/AlgorithmVisualizer.png',
+        imgURL: '/assets/AlgorithmVisualizer.jpg',
         stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'animejs'],
         link: 'https://kaiyan-da-man.github.io/algorithm-visualizer/',
     },
     {
         title: 'Developer Portfolio',
-        imgURL: '',
+        imgURL: '/assets/AlgorithmVisualizer.jpg',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://kaiyan-da-man.github.io/Developer-Portfolio/',
     },
