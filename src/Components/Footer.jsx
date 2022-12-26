ab899b77367e18ab3005fab8fcc56ab1a408a22b
Bryan Tang <bryanktang@gmail.com>
2022-12-26 09:08:56 -0600

    Add footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='py-5 text-center'>
+      {/* social icons */}
+      <p className='text-sm mt-2 opacity-50'>
+        &copy; {new Date().getFullYear()} Bryan Tang. All rights reserved.
+      </p>
+    </div>
   )
 }
