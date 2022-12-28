594116e3248e2dff6f0b9fc146bb3000a8bdca53
Bryan Tang <bryanktang@gmail.com>
2022-12-28 10:01:57 -0600

    Remove darkmodejs

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/tailwind.css'
-import './styles/darkmodejs.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
