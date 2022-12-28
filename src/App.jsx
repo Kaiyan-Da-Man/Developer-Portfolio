d65ed8c9761af05178fbc4c3ef29c2686b8ae509
Bryan Tang <bryanktang@gmail.com>
2022-12-28 09:58:35 -0600

    Remove darkmodejs

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,11 @@ import Footer from './Components/Footer'
 import Intro from './Components/Intro'
 import Portfolio from './Components/Portfolio'
 import Timeline from './Components/Timeline'
-import DarkMode from 'darkmode-js'
 
 function App() {
-  const options = {
-    bottom: '64px', // default: '32px'
-    right: '32px', // default: '32px'
-    left: 'unset', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: true, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
-  }
-  
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
   return (
     <>
-      <div className="bg-white text-stone-900 min-h-screen font-inter">
+      <div className="background bg-springGreen-50 text-stone-900 min-h-screen font-inter">
         <div className='max-w-5xl w-11/12 mx-auto'>
           <Intro />
           <Portfolio />
