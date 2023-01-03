27908e81ad9f957dc901ed65c87490a64ef19716
Bryan Tang <bryanktang@gmail.com>
2023-01-03 10:51:07 -0600

    Implement background gradient and edited text

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Intro() {
     return (
-        <div className='flex items-center justify-center flex-col text-center text-teal-700 pt-20 pb-6 dark:text-lightYellow width-full h-screen'>
-            <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Bryan Tang</h1>
-            <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer & Web Developer</p>
-            <p className='text-sm max-w-xl mb-6 font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate beatae velit laboriosam, explicabo exercitationem. Repudiandae ipsum in quas perferendis delectus voluptate, nam beatae! Exercitationem expedita provident unde perspiciatis ipsa?</p>
+        <div className="flex flex-col text-myTeal dark:text-lightYellow width-full h-screen justify-center">
+            <h1 className='text-xl md:text-3xl mb-1 md:mb-3 font-bold dark:bg-myTeal/50 bg-lightYellow-darkest/50 rounded-lg px-5 py-3 left-50 top-50 w-fit h-fit'>Hi there! I'm Bryan</h1>
+            <p className='text-base md:text-2xl mb-3 font-bold mt-10 ml-10'>Software Engineer & Web Developer</p>
+            <p className='text-sm w-1/2 md:text-2xl mb-6 font-medium mt-10 ml-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate beatae velit laboriosam, explicabo exercitationem. Repudiandae ipsum in quas perferendis delectus voluptate, nam beatae! Exercitationem expedita provident unde perspiciatis ipsa? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente quo aspernatur eaque omnis eveniet velit eum quasi perspiciatis consequatur, nemo molestias neque iure quod, non veritatis! A vel obcaecati corrupti. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolore a sunt, placeat rerum, voluptatem asperiores provident, repellendus doloribus alias voluptate aperiam amet corrupti eligendi obcaecati omnis quasi non facilis?</p>
         </div>
     )
 }
