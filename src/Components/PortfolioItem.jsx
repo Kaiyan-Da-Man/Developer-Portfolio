d8b2521e0627e981d6d19067854778bf1cc3126c
Bryan Tang <bryanktang@gmail.com>
2023-01-05 10:38:47 -0600

    update

--- src/Components/PortfolioItem.jsx
+++ src/Components/PortfolioItem.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 
 export default function PortfolioItem({ title, imgURL, stack, link }) {
-  return (
-    // each individual Portfolio item
-    <div className='flex w-full gap-10'>
-        <div className='bg-myTeal/50 rounded-md overflow-hidden hover:scale-110 ease-in duration-300 my-5'>
-            <a href={link} target='_blank'>
-            <img 
-                src={imgURL} 
-                alt="Project Image"
-                className='w-full h-36 md:h-60 object-cover cursor-pointer scale-95 rounded-lg'
-            />
-            </a>
+    return (
+        // each individual Portfolio item
+        <div className='flex flex-row w-full gap-10'>
+            <div className='bg-myTeal/50 rounded-md overflow-hidden hover:scale-110 ease-in duration-300 my-5'>
+                <a href={link} target='_blank'>
+                <img 
+                    src={imgURL} 
+                    alt="Project Image"
+                    className='w-full h-36 md:h-60 object-cover cursor-pointer scale-95 rounded-lg'
+                />
+                </a>
+            </div>
+            <div className='flex flex-col '>
+                {/* <div>Title</div>
+                <div>Languages</div>
+                <div>Description</div> */}
+                {/* stack */}
+                <div className='w-full p-4'>
+                    <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-springGreen-50'>{title}</h3>
+                    <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-springGreen-50'>
+                        {/* iterate through stack and output each element */}
+                        {stack.map(item => (
+                            <span className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
+                                {item}
+                            </span>
+                        ))}
+                    </p>
+                </div> 
+            </div>
         </div>
-        <div className='flex flex-col '>
-            {/* <div>Title</div>
-            <div>Languages</div>
-            <div>Description</div> */}
-            {/* stack */}
-            <div className='w-full p-4'>
-                <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-springGreen-50'>{title}</h3>
-                <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-springGreen-50'>
-                    {/* iterate through stack and output each element */}
-                    {stack.map(item => (
-                        <span className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md'>
-                            {item}
-                        </span>
-                    ))}
-                </p>
-            </div> 
-        </div>
-    </div>
-  )
+    )
 }
