8320bba2b7fcc38043b45ef2bea50532d13d8e11
Bryan Tang <bryanktang@gmail.com>
2023-01-05 10:20:04 -0600

    Add new layout

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -5,20 +5,41 @@ import PortfolioItem from './PortfolioItem'
 export default function Portfolio() {
   return (
     <>
-      <div className='flex flex-col items-center justify-center text-myTeal dark:text-lightYellow'>
-        <h1 className='text-xl md:text-7xl font-bold dark:bg-myTeal/50 bg-lightYellow-darkest/50 rounded-lg px-5 py-3 w-fit h-fit my-10'>Projects</h1>
-        <div className='md:flex-row'>
-          <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-            {portfolio.map(project => (
-              <PortfolioItem 
-                title={project.title}
-                imgURL={project.imgURL}
-                stack={project.stack}
-                link={project.link}
-              />
-            ))}
-          </div>
+      <div className='flex flex-col items-start text-myTeal dark:text-lightYellow'>
+        <h1 className='text-xl md:text-7xl font-bold dark:bg-myTeal/50 bg-lightYellow-darkest/50 rounded-lg px-5 py-3 w-fit h-fit my-10 self-center'>Projects</h1>
+        {/* <div className='flex flex-row gap-10 justify-between w-full'>
+          <div>picture</div>
+          <div>something</div>
         </div>
+        <div className='flex flex-row gap-10 justify-between w-full'>
+          <div>something</div>
+          <div>picture</div>
+        </div>
+        <div className='flex flex-row gap-10 justify-between w-full'>
+          <div>picture</div>
+          <div>something</div>
+        </div>
+        <div className='flex flex-row gap-10 justify-between w-full'>
+          <div>something</div>
+          <div>picture</div>
+        </div>
+        <div className='flex flex-row gap-10 justify-between w-full'>
+          <div>picture</div>
+          <div>something</div>
+        </div>
+        <div className='flex flex-row gap-10 justify-between w-full'>
+          <div>something</div>
+          <div>picture</div>
+        </div> */}
+        
+        {portfolio.map(project => (
+          <PortfolioItem 
+            title={project.title}
+            imgURL={project.imgURL}
+            stack={project.stack}
+            link={project.link}
+          />
+        ))}  
       </div>
     </>
   )
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -11,4 +11,28 @@ export default [
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://kaiyan-da-man.github.io/Developer-Portfolio/',
     },
+    {
+        title: 'Algorithm Visualizer',
+        imgURL: '/Developer-Portfolio/assets/img/AlgorithmVisualizer.jpg',
+        stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'animejs'],
+        link: 'https://kaiyan-da-man.github.io/algorithm-visualizer/',
+    },
+    {
+        title: 'Developer Portfolio',
+        imgURL: '/Developer-Portfolio/assets/img/Portfolio.png',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'https://kaiyan-da-man.github.io/Developer-Portfolio/',
+    },
+    {
+        title: 'Algorithm Visualizer',
+        imgURL: '/Developer-Portfolio/assets/img/AlgorithmVisualizer.jpg',
+        stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'animejs'],
+        link: 'https://kaiyan-da-man.github.io/algorithm-visualizer/',
+    },
+    {
+        title: 'Developer Portfolio',
+        imgURL: '/Developer-Portfolio/assets/img/Portfolio.png',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'https://kaiyan-da-man.github.io/Developer-Portfolio/',
+    },
 ]
