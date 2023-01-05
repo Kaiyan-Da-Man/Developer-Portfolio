69a0cc56f50c18a1250ce5eb64adc97b71fc86d2
Bryan Tang <bryanktang@gmail.com>
2023-01-05 11:28:12 -0600

    Added more stuff to portfolio and timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,26 +1,32 @@
 export default [
     {
-        year: 'Year',
-        title: 'Title',
-        duration: 'Duration',
+        year: 'Today',
+        title: 'Educational Mentor',
+        duration: '1 year',
         details: 'Details',
     },
     {
-        year: 'Year',
-        title: 'Title',
-        duration: 'Duration',
+        year: '2024',
+        title: 'Bachelor of Science in Computer Science',
+        duration: '4 years',
+        details: 'University of North Texas',
+    },
+    {
+        year: '2021',
+        title: 'Associates of Arts',
+        duration: '4 years',
+        details: 'Dual Credit',
+    },
+    {
+        year: '2020',
+        title: 'Kerr Dining Hall Student Staff',
+        duration: '4 months',
         details: 'Details',
     },
     {
-        year: 'Year',
-        title: 'Title',
-        duration: 'Duration',
-        details: 'Details',
-    },
-    {
-        year: 'Year',
-        title: 'Title',
-        duration: 'Duration',
+        year: '2016',
+        title: 'Hello, World',
+        duration: '',
         details: 'Details',
     },
 ]
