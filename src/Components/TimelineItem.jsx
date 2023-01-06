d7cd1cfcea480fa1cc02c75e1f6efcf6aa73bed1
Bryan Tang <bryanktang@gmail.com>
2023-01-06 10:59:39 -0600

    change style

--- src/Components/TimelineItem.jsx
+++ src/Components/TimelineItem.jsx
@@ -2,23 +2,26 @@ import React from 'react'
 
 export default function TimelineItem({ year, title, duration, details }) {
   return (
-    <ol className='flex flex-col md:flex-row relative border-l border-stone-200'>
+    <ol className='flex flex-col md:flex-row relative border-l border-myTeal dark:border-lightYellow'>
         <li className='mb-10 ml-4'>
-            <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white' />
-            <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
-                <span className='inline-block px-2 font-semibold text-white bg-stone-900 rounded-md'>
-                    {year}
-                </span>
-                <h3 className='text-lg font-semibold text-stone-900'>
-                    {title}
-                </h3>
-                <div className='my-1 text-sm font-normal- leading-none text-stone-400'>
-                    {duration}
-                </div>
-            </p>
-            <p className='my-2 text-base font-normal text-stone-500'>
-                {details}
-            </p>
+            <div className='absolute w-3 h-3 bg-myTeal dark:bg-lightYellow rounded-full mt-1.5 -left-1.5 border border-lightYellow dark:border-myTeal' />
+            <div className='ease-in duration-300 hover:bg-myTeal-light/50 hover:p-10 rounded-lg'>
+                <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
+                    <span className='inline-block px-2 font-semibold text-lightYellow bg-myTeal dark:text-myTeal dark:bg-lightYellow rounded-md'>
+                        {year}
+                    </span>
+                    <h3 className='text-lg font-semibold text-myTeal dark:text-lightYellow'>
+                        {title}
+                    </h3>
+                    <div className='my-1 text-sm font-normal leading-none text-myTeal dark:text-lightYellow'>
+                        {duration}
+                    </div>
+                </p>
+                <p className='my-2 text-base font-normal text-myTeal dark:text-lightYellow'>
+                    {details}
+                </p>    
+            </div>
+            
         </li>
     </ol>
   )
